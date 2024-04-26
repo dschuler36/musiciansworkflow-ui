936afc91e0d94264c9db1ab42f32701acba88de3
David Schuler <dschuler36@gmail.com>
2024-04-26 19:58:21 -0400

    tracks page updates

--- src/components/Tracks.vue
+++ src/components/Tracks.vue
@@ -5,9 +5,11 @@
     <br/>
 
     <!-- Button to open new track dialog -->
-    <v-btn color="primary" dark @click="showDialog = true">
-      + New Track
-    </v-btn>
+    <div class="button-container">
+      <v-btn color="primary" dark @click="showDialog = true">
+        + New Track
+      </v-btn>
+    </div>
 
     <!-- Dialog for new track -->
     <v-dialog v-model="showDialog" persistent max-width="600px">
@@ -39,9 +41,11 @@
       <!-- Goals Form Dialog -->
       <v-dialog v-model="goalsDialog" persistent max-width="600px">
         <v-card>
-          <v-card-title>Enter Goals for {{ newTrack.name }}</v-card-title>
+          <v-card-title>Stay on track. Enter goals for {{ newTrack.name }}</v-card-title>
           <v-card-text>
-            <v-text-field v-for="stage in stages" :key="stage" :label="`${stage} Goal (YYYY-MM-DD)`" v-model="newGoals[stage]"></v-text-field>
+            <v-text-field v-for="stage in stages" :key="stage" :label="`${stage} Goal (YYYY-MM-DD)`" v-model="newGoals[`${stage.toLowerCase()}_goal`]"></v-text-field>
+
+            <!-- <v-text-field v-for="stage in stages" :key="stage" :label="`${stage} Goal (YYYY-MM-DD)`" v-model="newGoals[`${stage.toLowerCase()}_goal`]"></v-text-field> -->
           </v-card-text>
           <v-card-actions>
             <v-spacer></v-spacer>
@@ -51,37 +55,43 @@
       </v-dialog>
 
     <!-- Existing tracks list -->
-    <v-data-iterator :items="tracks" :page.sync="page" :items-per-page="5">
-      <template v-slot:default="{ items }">
-        <div v-for="(item, i) in tracks" :key="i">
-          <v-card>
-            <!-- Row for card content -->
-            <v-row align="center">
-              <!-- Track Name -->
-              <v-col cols="4">
-                <router-link :to="{ name: 'TrackDetail', params: { trackId: item.track_id }}">
-                  <v-card-title class="justify-start">{{ item.name }}</v-card-title>
-                </router-link>
-              </v-col>
-              
-              <!-- Stage and Goal -->
-              <v-col cols="4">
-                <v-card-text>Stage: {{ item.stage }}</v-card-text>
-                <v-card-text>Goal: {{ item.goal }}</v-card-text>
-              </v-col>
-              
-              <!-- Release and Album Art -->
-              <v-col cols="4">
-                <v-card-text class="text-right">{{ item.release }}</v-card-text>
-                <v-img :src="item.albumArt" aspect-ratio="1.7" contain></v-img>
-              </v-col>
-            </v-row>
-          
-          </v-card>
-          <br>
-        </div>
-      </template>
-    </v-data-iterator>
+    <div class="card-container">
+      <v-data-iterator :items="tracks" :page.sync="page" :items-per-page="5">
+        <template v-slot:default="{ items }">
+          <div v-for="(item, i) in tracks" :key="i">
+            <v-card class="track-card">
+              <v-container fluid>
+                <v-row no-gutters align="center">
+                  <!-- Album Art -->
+                  <v-col cols="2">
+                    <v-img :src="item.albumArt" contain></v-img>
+                  </v-col>
+
+                  <!-- Track Info -->
+                  <v-col cols="8">
+                    <div class="track-info">
+                      <router-link :to="{ name: 'TrackDetail', params: { trackId: item.track_id }}">
+                        <span class="track-name">{{ item.name }}</span>
+                      </router-link>
+                        <div class="release-and-stage">
+                        <span class="release-name">{{ item.release }}</span>
+                        <v-chip :color="getStageColor(item.stage)" class="stage-chip">
+                          {{ item.stage }}
+                        </v-chip>
+                      </div>
+                    </div>
+                  </v-col>
+                  <v-col cols="2" class="last-updated">
+                    {{ item.lastUpdated }}
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-card>
+            <br>
+          </div>
+        </template>
+      </v-data-iterator>
+    </div>
   </div>
 </template>
 
@@ -97,17 +107,18 @@ export default {
     const tracks = ref([]);
     const page = ref(1);
     const showDialog = ref(false);
-    const stages = ['Writing', 'Recording', 'Mixing', 'Mastering', 'Released'];
+    const stages = ['Writing', 'Recording', 'Mixing', 'Mastering', 'Release'];
     const newTrack = ref({
+      track_id: '',
       name: '',
       release: '',
       stage: '',
       goals: {
-        Writing: '',
-        Recording: '',
-        Mixing: '',
-        Mastering: '',
-        Released: ''
+        writing_goal: '',
+        recording_goal: '',
+        mixing_goal: '',
+        mastering_goal: '',
+        release_goal: ''
       }
     });
     const trackDialog = ref(true);  // Initially show track dialog
@@ -118,6 +129,13 @@ export default {
     const goalsInputVisible = computed(() => {
       return newTrack.value.stage !== 'Released';
     });
+
+    const initializeNewGoals = () => {
+      newGoals.value = {};
+      stages.forEach(stage => {
+        newGoals.value[`${stage.toLowerCase()}_goal`] = '';
+      });
+    };
 
     const fetchTracks = async () => {
       try {
@@ -135,13 +153,17 @@ export default {
           release: newTrack.value.release,
           stage: newTrack.value.stage
         };
-        await axios.post(`/users/${userId.value}/tracks`, trackData);
-        trackDialog.value = false;
-        stages.forEach(stage => { newGoals.value[stage] = ''; });  // Prepare goals form
-        trackDialog.value = false;
+        console.log('new track before posting', newTrack.value);
+        const response = await axios.post(`/users/${userId.value}/tracks`, trackData);
+        newTrack.value.track_id = response.data.track_id;
+        showDialog.value = false;
+        initializeNewGoals();
+        // console.log('new track after posting', newTrack);
+        // trackDialog.value = false;
+        // stages.forEach(stage => { newGoals.value[stage] = ''; });  // Prepare goals form
+        // trackDialog.value = false;
         goalsDialog.value = true;  // Open goals dialog
         fetchTracks();
-        resetNewTrack();
       } catch (error) {
         console.error('Failed to submit new track:', error);
       }
@@ -149,46 +171,83 @@ export default {
 
     const resetNewTrack = () => {
       newTrack.value = {
+        track_id: '',
         name: '',
         release: '',
         stage: '',
         goals: {
-          Writing: '',
-          Recording: '',
-          Mixing: '',
-          Mastering: '',
-          Released: ''
+          writing_goal: '',
+          recording_goal: '',
+          mixing_goal: '',
+          mastering_goal: '',
+          release_goal: ''
         }
       };
     };
 
-    const submitGoals = async () => {
-  // Check if any goals have been provided
-      const hasGoals = Object.values(newGoals.value).some(goal => goal.trim() !== '');
-
-      if (hasGoals) {
-        try {
-          await axios.post(`/tracks/${newTrack.value.id}/goals`, { goals: newGoals.value });
-          console.log("Goals saved successfully");
-        } catch (error) {
-          console.error('Error submitting goals:', error);
-        }
-      } else {
-        console.log("No goals provided, skipping goal submission.");
+  const submitGoals = async () => {
+    const goalsPayload = {};
+    
+    // Filter out empty goals and build the payload with correct keys
+    Object.entries(newGoals.value).forEach(([key, value]) => {
+      if (value.trim() !== '') {
+        goalsPayload[key] = value;
       }
+    });
+    console.log(goalsPayload);
 
-      // Close the goals dialog regardless of whether an API call was made
-      goalsDialog.value = false;
-    };
+    // Send the goals if there are any non-empty entries
+    if (Object.keys(goalsPayload).length > 0) {
+      try {
+        await axios.post(`/tracks/${newTrack.value.track_id}/goals`, goalsPayload );
+        console.log("Goals saved successfully");
+      } catch (error) {
+        console.error('Error submitting goals:', error);
+      }
+    } else {
+      console.log("No goals provided, skipping goal submission.");
+    }
+
+    // Reset dialog states
+    showDialog.value = false; // Close track creation dialog
+    goalsDialog.value = false; // Close goals dialog
+    resetNewTrack();
+  };
 
     onMounted(fetchTracks);
 
     return { page, tracks, showDialog, newTrack, submitNewTrack, goalsInputVisible, stages,
               submitGoals, trackDialog, goalsDialog, newGoals };
+  },
+  methods: {
+    getStageColor(stage) {
+    switch (stage) {
+      case 'Writing':
+        return 'blue';
+      case 'Recording':
+        return 'red';
+      case 'Mixing':
+        return 'orange';
+      case 'Mastering':
+        return 'yellowgreen';
+      case 'Release':
+        return 'green';
+      default:
+        return 'grey';
+    }
   }
+},
+computed: {
+  stageClass() {
+    const stage = this.item.stage.toLowerCase();
+    const validStages = ['writing', 'recording', 'mixing', 'mastering', 'released'];
+    return validStages.includes(stage) ? `stage-${stage}` : 'stage-default';
+  }
+}
 };
+
 </script>
-  
+
 <style scoped>
 .justify-start {
   justify-content: flex-start;
@@ -199,15 +258,123 @@ export default {
 }
 
 /* Optional: Additional styling for overall card layout */
+/* .v-card {
+  overflow: hidden;
+} */
+
+.card-container {
+  padding: 0 20px; /* Adjusts the padding on both sides */
+  width: 100%; /* Takes full width of the viewport */
+}
+
 .v-card {
-  overflow: hidden; /* Ensures the content fits within the card */
+  width: 100%; /* Ensures cards stretch to fill the container */
+  box-shadow: 0 4px 8px rgba(0,0,0,0.12);
+  border: 1px solid #e0e0e02d; /* Light grey border */
+  border-radius: 8px;
+  margin-bottom: 20px; /* Space between cards */
+  max-width: 1200px; /* Maximum width of the card */
+  margin-left: auto; /* Centers the card horizontally */
+  margin-right: auto;
+}
+
+@media (max-width: 600px) {
+  .card-container {
+    padding: 0 10px; /* Less padding on smaller screens */
+  }
+}
+.v-card:hover {
+  box-shadow: 0 8px 16px rgba(0,0,0,0.15); /* Enhanced shadow on hover */
 }
 
 .v-card-title, .v-card-text {
   font-size: 0.875rem; /* Smaller font size */
 }
 
-.v-img {
-  max-height: 50px; /* Restricting image height can also help */
+.button-container {
+  display: flex;
+  justify-content: center; /* Aligns the button to the center horizontally */
+  margin-top: 20px; /* Optional: Adds some space above the button */
+  margin-bottom: 20px;
 }
+.track-info {
+  display: block;
+}
+
+.track-name {
+  font-weight: bold;
+  font-size: 1.2em;
+  margin-bottom: 4px; /* Adds space below the track name */
+  cursor: pointer; /* Changes cursor to indicate clickable */
+  text-decoration: none; /* No underline by default */
+  color: inherit; /* Keeps the text color consistent */
+}
+
+.release-and-stage {
+  display: flex; /* Establishes a flex container */
+  align-items: center; /* Aligns children vertically */
+  gap: 8px; /* Adds space between the release name and the stage chip */
+}
+
+.release-name {
+  font-size: 0.9em;
+  color: #757575; /* Less emphasis on the release name */
+}
+
+.stage-chip {
+  font-size: 0.75rem;
+  padding: 4px 8px;
+  border-radius: 10px;
+  line-height: 1;
+  margin: 0; /* Removes any default margin if needed */
+}
+
+
+.v-chip {
+  font-size: 0.75rem; /* Smaller font size */
+  padding: 4px 8px; /* Reduced padding */
+  border-radius: 10px; /* Adjusted border radius for a rounded look */
+  line-height: 1; /* Adjust line height to control the height of the chip */
+  margin-right: 4px; /* Space to the right of the chip */
+  margin-bottom: 4px; /* Space below the chip */
+}
+
+.last-updated {
+  text-align: right;
+  font-size: 0.8em;
+  color: #757575; /* Grey text for less emphasis */
+}
+
+/* For the album art */
+.v-img {
+  max-width: 80px; /* Set a max-width if necessary */
+  border-radius: 50%; /* Circular image */
+}
+
+.stage-writing {
+  background-color: blue;
+}
+
+.stage-recording {
+  background-color: plum;
+}
+
+.stage-mixing {
+  background-color: orange;
+}
+
+.stage-mastering {
+  background-color: yellowgreen;
+}
+
+.stage-release {
+  background-color: green;
+}
+
+/* Fallback for any stage not explicitly defined */
+.stage-default {
+  background-color: grey;
+}
+
+
 </style>
